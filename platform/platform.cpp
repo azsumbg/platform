2bfab21cc30e6ac201584fb3f2b996305bb84105
Daniel Dinev <azsumbg@gmail.com>
2024-10-30 08:54:48 +0200

    Added EVILS definition

--- platform/platform.cpp
+++ platform/platform.cpp
@@ -114,6 +114,26 @@ gamedll::FIELD::FIELD(fields what_type) :ATOM(scr_width, 50.0f, 1000.0f, 750.0f)
 {
 	type = what_type;
 	dir = dirs::left;
+	if (type == fields::platform)NewDims(100.0f, 50.0f);
+
+	switch (type)
+	{
+	case fields::field1:
+		max_frames = 10;
+		frame_delay = 5;
+		break;
+
+	case fields::field2:
+		max_frames = 7;
+		frame_delay = 8;
+		break;
+
+	case fields::field3:
+		max_frames = 34;
+		frame_delay = 2;
+		break;
+	}
+
 }
 void gamedll::FIELD::Release()
 {
@@ -159,5 +179,251 @@ void gamedll::FIELD::Move(float gear)
 		}
 	}
 }
+int gamedll::FIELD::GetFrame()
+{
+	frame_delay--;
+	if (frame_delay <= 0)
+	{
+		switch (type)
+		{
+		case fields::field1:
+			frame_delay = 5;
+			break;
+
+		case fields::field2:
+			frame_delay = 8;
+			break;
+
+		case fields::field3:
+			frame_delay = 2;
+			break;
+		}
+		++frame;
+		if (frame > max_frames)frame = 0;
+	}
+	return frame;
+}
 
 /////////////////////////////
+
+//CREATURE BASE CLASS *************
+
+gamedll::CREATURE::CREATURE(creatures what, float start_x, float start_y) :ATOM(start_x, start_y)
+{
+	type = what;
+	dir = dirs::left;
+
+	switch (type)
+	{
+	case creatures::evil1:
+		NewDims(45.0f, 55.0f);
+		speed = 0.5f;
+		lifes = 50;
+		max_frames = 23;
+		frame_delay = 3;
+		strenght = 10;
+		attack_delay = 20;
+		break;
+
+	case creatures::evil2:
+		NewDims(22.0f, 53.0f);
+		speed = 0.8f;
+		lifes = 30;
+		max_frames = 23;
+		frame_delay = 3;
+		strenght = 5;
+		attack_delay = 15;
+		break;
+
+	case creatures::evil3:
+		NewDims(35.0f, 54.0f);
+		speed = 0.6f;
+		lifes = 40;
+		max_frames = 23;
+		frame_delay = 3;
+		strenght = 8;
+		attack_delay = 25;
+		break;
+
+	case creatures::evil4:
+		NewDims(40.0f, 41.0f);
+		speed = 0.8f;
+		lifes = 45;
+		max_frames = 23;
+		frame_delay = 3;
+		strenght = 8;
+		attack_delay = 10;
+		break;
+
+	case creatures::evil5:
+		NewDims(36.0f, 66.0f);
+		speed = 0.6f;
+		lifes = 35;
+		max_frames = 23;
+		frame_delay = 3;
+		strenght = 10;
+		attack_delay = 25;
+		break;
+
+	case creatures::evil6:
+		NewDims(43.0f, 55.0f);
+		speed = 0.7f;
+		max_frames = 23;
+		frame_delay = 3;
+		strenght = 12;
+		attack_delay = 25;
+		break;
+
+	case creatures::hero:
+		NewDims(47.0f, 47.0f);
+		dir = dirs::right;
+		speed = 10.0f;
+		max_frames = 23;
+		frame_delay = 3;
+		strenght = 20;
+		attack_delay = 10;
+		break;
+	}
+}
+void gamedll::CREATURE::SetPathInfo(float where_x, float where_y)
+{
+	start_x = x; 
+	start_y = y;
+	end_x = where_x;
+	end_y = where_y;
+
+	vert_path = false;
+	hor_path = false;
+
+	if (end_x - start_x == 0)
+	{
+		vert_path = true;
+		return;
+	}
+	if (end_y - start_y == 0)
+	{
+		hor_path = true;
+		return;
+	}
+
+	slope = (end_y - start_y) / (end_x - start_x);
+	intercept = start_y - start_x * slope;
+}
+creatures gamedll::CREATURE::GetType() const
+{
+	return type;
+}
+int gamedll::CREATURE::Attack()
+{
+	attack_cooldown++;
+	if (attack_cooldown > attack_delay)
+	{
+		attack_delay = 0;
+		return strenght;
+	}
+	return 0;
+}
+bool gamedll::CREATURE::Falling(CONTAINER& platforms)
+{
+	if (platforms.size() < 1)return true;
+	for (int i = 0; i < platforms.size(); i++)
+	{
+		if (!(x > platforms[i].ex || ex < platforms[i].x || y > platforms[i].ey || ey < platforms[i].y))
+		{
+			if ((dir == dirs::up || dir == dirs::down) && y <= platforms[i].y)return false;
+		}
+	}
+	return true;
+}
+
+///////////////////////////////////
+
+//EVILS **********************
+
+void gamedll::EVILS::Release()
+{
+	delete this;
+}
+bool gamedll::EVILS::GetFlag(unsigned char which_flag) const 
+{
+	return flags & which_flag;
+}
+void gamedll::EVILS::SetFlag(unsigned char which_flag)
+{
+	flags |= which_flag;
+}
+void gamedll::EVILS::NullFlag(unsigned char which_flag)
+{
+	flags &= ~which_flag;
+}
+int gamedll::EVILS::GetFrame()
+{
+	--frame_delay;
+	if (frame_delay <= 0)
+	{
+		frame_delay = 3;
+		++frame;
+		if (frame > max_frames)frame = 0;
+	}
+	return frame;
+}
+unsigned char gamedll::EVILS::Move(float gear, bool change_path, float path_ex, float path_ey)
+{
+	flags = 0b00000000;
+	if (change_path)SetPathInfo(path_ex, path_ey);
+	float current_speed = speed + gear / 10;
+
+	if (vert_path)
+	{
+		if (dir == dirs::up)
+		{
+			y -= current_speed;
+			SetEdges();
+			if (ey <= sky)SetFlag(UP_FLAG);
+		}
+		else if (dir == dirs::down)
+		{
+			y += current_speed;
+			SetEdges();
+			if (y >= ground)SetFlag(DOWN_FLAG);
+		}
+	}
+	else if (hor_path)
+	{
+		if (dir == dirs::left)
+		{
+			x -= current_speed;
+			SetEdges();
+			if (ex <= -(2 * scr_width))SetFlag(LEFT_FLAG);
+		}
+		else if (dir == dirs::right)
+		{
+			x += current_speed;
+			SetEdges();
+			if (x >= 2 * scr_width)SetFlag(RIGHT_FLAG);
+		}
+	}
+	else
+	{
+		if (dir == dirs::left)
+		{
+			x -= current_speed;
+			y = x * slope + intercept;
+			SetEdges();
+			if (ex <= -(2 * scr_width))SetFlag(LEFT_FLAG);
+		}
+		else if (dir == dirs::right)
+		{
+			x += current_speed;
+			y = x * slope + intercept;
+			SetEdges();
+			if (ex <= 2 * scr_width)SetFlag(RIGHT_FLAG);
+		}
+	}
+
+	return flags;
+}
+
+///////////////////////////////
+
+
--- platform/platform.h
+++ platform/platform.h
@@ -19,13 +19,15 @@ constexpr char UP_FLAG{ 0b00000001 };
 constexpr char DOWN_FLAG{ 0b00000010 };
 constexpr char LEFT_FLAG{ 0b00000100 };
 constexpr char RIGHT_FLAG{ 0b00001000 };
+constexpr char JUMP_FLAG{ 0b00010000 };
+constexpr char FALL_FLAG{ 0b00100000 };
 
 enum class dirs { up = 0, down = 1, left = 2, right = 3, stop = 4 };
 enum class creatures {
 	evil1 = 0, evil2 = 1, evil3 = 2, evil4 = 3,
 	evil5 = 4, evil6 = 5, hero = 6
 };
-enum class fields { field1 = 0, field2 = 1, field3 = 2, field4 = 3, field5 = 4 };
+enum class fields { field1 = 0, field2 = 1, field3 = 2, platform = 3 };
 
 namespace gamedll
 {
@@ -99,6 +101,10 @@ namespace gamedll
 			fields type{};
 			unsigned char flags{ 0 };
 
+			int frame{ 0 };
+			int max_frames{ 0 };
+			int frame_delay{ 0 };
+
 		public:
 			dirs dir{};
 			float speed{ 0 };
@@ -108,11 +114,82 @@ namespace gamedll
 
 			void Release();
 			fields GetType() const;
+			int GetFrame();
 
 			bool GetFlag(unsigned char which_flag) const;
 			void SetFlag(unsigned char which_flag);
 			void NullFlag(unsigned char which_flag);
 			void Move(float gear);
 	};
+
+	class PLATFORM_API CREATURE :public ATOM
+	{
+		protected:
+			unsigned char flags{ 0 };
+
+			float start_x{};
+			float start_y{};
+			float end_x{};
+			float end_y{};
+			
+			float slope{};
+			float intercept{};
+			float speed{ 1.0f };
+
+			bool vert_path = false;
+			bool hor_path = false;
+
+			int max_frames{};
+			int frame{};
+			int frame_delay{};
+
+			int strenght{ 0 };
+			int attack_delay{ 0 };
+			int attack_cooldown{ 0 };
+
+			creatures type{};
+
+			void SetPathInfo(float where_x, float where_y);
+			
+		public:
+			dirs dir{};
+			int lifes{ 0 };
+
+			CREATURE(creatures what, float start_x, float start_y);
+			virtual ~CREATURE() {};
+
+			virtual void Release() = 0;
+			
+			virtual bool GetFlag(unsigned char which_flag) const = 0;
+			virtual void SetFlag(unsigned char which_flag) = 0;
+			virtual void NullFlag(unsigned char which_flag) = 0;
+
+			creatures GetType() const;
+			
+			int Attack();
+			bool Falling(CONTAINER& platforms);
+
+			virtual int GetFrame() = 0;
+
+			virtual unsigned char Move(float gear, bool change_path = false, float path_ex = 0, float path_ey = 0) = 0;
+	};
+
+	class PLATFORM_API EVILS :public CREATURE
+	{
+		protected:
+			EVILS(creatures what, float _x, float _y) :CREATURE(what, _x, _y) {};
+
+		public:
+			friend CREATURE* CreatureFactory(creatures which_creature, float start_x, float start_y);
+
+			void Release() override;
+			bool GetFlag(unsigned char which_flag) const override;
+			void SetFlag(unsigned char which_flag) override;
+			void NullFlag(unsigned char which_flag) override;
+			int GetFrame() override;
+			unsigned char Move(float gear, bool change_path, float path_ex, float path_ey) override;
+	};
+
+	typedef CREATURE* Creature;
 
 }
